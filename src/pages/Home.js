cb4fe412bfde641ff5af52c741b36af4ec39846c
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-06-13 16:12:44 +0100

    Search

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import { Box } from '@mui/material';
 
 import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
 
 const Home = () => {
 
   return (
     <Box>
       <HeroBanner />
+      <SearchExercises  />
     </Box>
   );
 };
