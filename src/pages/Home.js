bc108e46131fb3aa7ca62b4dd1019b2f82430b58
Nuno Miguel Ferraz <108nunomiguelferraz@gmail.com>
2022-06-13 16:27:09 +0100

    Home 2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,27 @@
-import React, { useState } from 'react';
-import { Box } from '@mui/material';
+import React, { useState } from "react";
+import { Box } from "@mui/material";
 
-import HeroBanner from '../components/HeroBanner';
-import SearchExercises from '../components/SearchExercises';
+import SearchExercises from "../components/SearchExercises";
+import HeroBanner from "../components/HeroBanner";
+import Exercises from "../components/Exercises";
 
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState("all");
 
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises  />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        exercises={exercises}
+        bodyPart={bodyPart}
+      />
     </Box>
   );
 };
